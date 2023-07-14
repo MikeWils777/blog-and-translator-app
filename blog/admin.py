6b3663a225da43b686f51180becc02fb8fd2f051
Michael Chiejine <mikewils.mc@gmail.com>
2023-07-14 14:50:43 +0100

    Initial commit

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-#class that controls view on the blog posts
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date_created', 'author')
-
-# Register your models here.
-admin.site.register(Post, PostAdmin)
